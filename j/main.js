a60fba9bd689f826bdfd37c1812924186ff1af0e
Tadeu <tadeulima14@gmail.com>
2023-11-20 21:26:31 -0800

    customized

---
+++ j/main.js
@@ -0,0 +1,40 @@
+// This code does NOT create any global variables.
+// Promises can be chained together, with the previous promise
+// passing its results to the next one in the chain.
+// The format is: fetch().then().then().catch()
+// It's easier to read if we put each step in its own line,
+// that's why the periods start the then lines.
+
+fetch("houses.json")
+    .then((response) => response.json())
+    .then((data) => {
+        // Populate the dropdown with the house names
+        const dropdown = document.getElementById("house-dropdown");
+        data.forEach((house) => {
+            let option = document.createElement("option");
+            option.value = house.code;
+            option.text = house.name;
+            dropdown.appendChild(option);
+        });
+
+        // Event listener for the dropdown
+        dropdown.addEventListener("change", (e) => {
+            const selectedHouse = e.target.value;
+            const container = document.querySelector("#container");
+            container.innerHTML = ""; // Clear previous content
+
+            // Find the selected house in the data
+            const selectedData = data.find((house) => house.code === selectedHouse);
+
+            // Display the members of the selected house
+            if (selectedData) {
+                let html = "";
+                selectedData.members.forEach((member) => {
+                    html += `<p>${member}</p>`;
+                });
+                container.innerHTML = html;
+            }
+        });
+    })
+    .catch((err) => console.log("Oops!", err));
+// This only runs if there is an error during the above process
